8e92e2b9db73ae9a0e2b78e66b8b0d15c0b73da2
stabah <stabah@cs.spbstu.ru>
2013-10-12 10:22:44 +0400

    Create const.py

---
+++ practice_1/const.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+
+""" константы"""
+
+dict = { 'pi' : '3.1434653464'}
+def constd(x,**kwargs)
+try:
+
+
+s = ' '
+for i in range(0,kwargs[x]):
+      s = s + dict[arg1][i]
+except:
+print 'error detected'
+return s
+    
